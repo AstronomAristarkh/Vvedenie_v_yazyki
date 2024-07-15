20b1c052243d5e04e47e52191fdd3bd027e37e27
astronom <chasovnikov.ar17@physics.msu.ru>
2024-07-15 19:39:42 +0300

    HW_2

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.ComponentModel.DataAnnotations;
+
+// Задача 1.
+//
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n % 7 == 0 && n % 23 == 0)
+//     Console.WriteLine("True");
+// else
+//     Console.WriteLine("False");
+
+//Задача 2.
+//
+// double x = Convert.ToDouble(Console.ReadLine());
+// double y = Convert.ToDouble(Console.ReadLine());
+// if (x>0)
+// {
+//     if (y>0)
+//         Console.WriteLine("I");
+//     else
+//         Console.WriteLine("IV"); 
+// }
+// else
+// {
+//     if (y>0)
+//         Console.WriteLine("II");
+//     else
+//         Console.WriteLine("III"); 
+// }
+
+
+//Задача 3.
+//
+// int n = Convert.ToInt32(Console.ReadLine());
+// int dec = n/10;
+// int unit = n%10;
+// if (dec > unit)
+//     Console.WriteLine(dec);
+// else
+//     Console.WriteLine(unit);
+
+//Задача 4.
+//
+// int n = Convert.ToInt32(Console.ReadLine());
+// while (n > 9)
+// {
+//     Console.Write(n % 10 + ", ");
+//     n = n / 10;
+// }
+// Console.Write(n % 10);

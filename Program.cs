3f8d1f3d2f6f188d40e7c57257b35a4bfb527d56
astronom <chasovnikov.ar17@physics.msu.ru>
2024-07-16 17:45:38 +0300

    HW_4

--- Program.cs
+++ Program.cs
@@ -49,3 +49,105 @@ using System.ComponentModel.DataAnnotations;
 //     n = n / 10;
 // }
 // Console.Write(n % 10);
+
+//Задача 4.1
+//
+// while (0 == 0)
+// {
+//     string? str = Console.ReadLine();
+//     if (str == "q")
+//         break;
+//     int n = Convert.ToInt32(str);
+//     int a = 0;
+//     while (n > 0)
+//     {
+//         a += n % 10;
+//         n = n / 10;    
+//     }
+//     if (a % 2 == 0)
+//         break;
+// }
+
+//Задача 4.2
+//
+// void Main()
+// {
+//     int arraySize = ReadInt("Введите размер массива: ");
+//     int[] array = GenerateArray(arraySize);
+//     PrintArray(array);
+//     Console.WriteLine(CountEven(array));
+// }
+
+// int CountEven(int[] arr)
+// {
+//     int count = 0;
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//     if (arr[i] % 2 == 0)
+//         count++;
+//     }
+//     return count;
+// }
+
+// void PrintArray(int[] arrayForPrint)
+// {
+//     for (int i = 0; i < arrayForPrint.Length; i++)
+//         System.Console.Write(arrayForPrint[i] + " ");
+//     System.Console.WriteLine();
+// }
+
+// int[] GenerateArray(int size)
+// {
+//     int[] tempArray = new int[size];
+//     Random rand = new Random();
+//     for (int i = 0; i < tempArray.Length; i++)
+//         tempArray[i] = rand.Next(100, 999);
+//     return tempArray;
+// }
+
+// int ReadInt(string msg)
+// {
+//     System.Console.Write(msg);
+//     return Convert.ToInt32(Console.ReadLine()); // 13
+// }
+// Main();
+
+//Задача 4.3
+//
+void Main()
+{
+    int arraySize = ReadInt("Введите размер массива: ");
+    int[] array = GenerateArray(arraySize);
+    PrintArray(array);
+    PrintAntiArray(array);
+}
+
+void PrintArray(int[] arrayForPrint)
+{
+    for (int i = 0; i < arrayForPrint.Length; i++)
+        System.Console.Write(arrayForPrint[i] + " ");
+    System.Console.WriteLine();
+}
+
+void PrintAntiArray(int[] arrayForPrint)
+{
+    for (int i = arrayForPrint.Length-1; i >= 0; i--)
+        System.Console.Write(arrayForPrint[i] + " ");
+    System.Console.WriteLine();
+}
+
+int[] GenerateArray(int size)
+{
+    int[] tempArray = new int[size];
+    Random rand = new Random();
+    for (int i = 0; i < tempArray.Length; i++)
+        tempArray[i] = rand.Next(100, 999);
+    return tempArray;
+}
+
+int ReadInt(string msg)
+{
+    System.Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine()); // 13
+}
+Main();
--- obj/Debug/net8.0/C#.AssemblyInfo.cs
+++ obj/Debug/net8.0/C#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("C#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20b1c052243d5e04e47e52191fdd3bd027e37e27")]
 [assembly: System.Reflection.AssemblyProductAttribute("C#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("C#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
